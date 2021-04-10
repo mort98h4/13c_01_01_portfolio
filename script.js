30363fdb72f21f10027b2979962e00a4eca78bd1
Morten Gross <mg@vuffeli.dk>
2021-04-10 14:17:58 +0200

    the fade in animation is done

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ window.addEventListener("DOMContentLoaded", init);
 function init() {
     console.log("init");
 
+    startAnimation();
+
     // JavaScript der styrer burgermenu
     const burger = document.querySelector('.burgerMenu');
     const nav = document.querySelector('.linksContainer');
 
     burger.addEventListener("mouseover", () => {
         document.querySelectorAll(".burger").forEach(element => {
-            element.style.backgroundColor = "var(--highCol)";
+            element.style.setProperty("background-color", "var(--highCol)");
         });
     });
 
     burger.addEventListener("mouseout", () => {
         document.querySelectorAll(".burger").forEach(element => {
-            element.style.backgroundColor = "var(--textCol)";
+            element.style.setProperty("background-color", "var(--textCol)");
         });
     });
 
@@ -28,4 +30,27 @@ function init() {
         // Tilføjer/fjerner "animation" på burgermenu
         burger.classList.toggle("toggle");
     });
+}
+
+function startAnimation() {
+    const blackOut = document.querySelector(".blackOut");
+
+    const blackProp = {
+        duration: 1000,
+        iterations: 1,
+        ease: "linear",
+        fill: "both"
+    }
+
+    const blackKF = {
+        opacity: [
+            "1",
+            "0"
+        ]
+    }
+
+    const blackAnimation = blackOut.animate(blackKF, blackProp);
+    blackAnimation.onfinish = function() {
+        blackOut.style.display = "none";
+    };
 }
