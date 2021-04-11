8be34777c82a2bc2e1c025026d12d68d933c7ba3
Morten Gross <mg@vuffeli.dk>
2021-04-11 22:33:09 +0200

    now has a splash section

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,15 +1,24 @@
 "use strict";
 
+const startTag = document.querySelector(".startTag").textContent;
+const endTag = document.querySelector(".endTag").textContent;
+
 import {burgerMenu} from "/../scripts/menu.js";
+import {fadeInAnimation} from "/../scripts/header_animation.js";
+import {scrollAnimation} from "/../scripts/index_scroll.js";
 import {changeIconColor} from "/../scripts/social_icons.js";
 
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
     console.log("init");
+    document.querySelector(".startTag").textContent = "";
+    document.querySelector(".endTag").textContent = "";
 
     gsap.registerPlugin(ScrollTrigger);
 
+    fadeInAnimation(startTag, endTag);
     burgerMenu();
+    scrollAnimation("#contact");
     changeIconColor();
 }
