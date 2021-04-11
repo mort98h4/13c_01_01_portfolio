45c5dedacfb14c61c7d24466141aba1fcaa530a5
Morten Gross <mg@vuffeli.dk>
2021-04-11 21:32:54 +0200

    splash section added

--- scripts/about.js
+++ scripts/about.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const startTag = document.querySelector(".startTag").textContent;
+const endTag = document.querySelector(".endTag").textContent;
+
+import { fadeInAnimation } from "/../scripts/header_animation.js";
 import {burgerMenu} from "/../scripts/menu.js";
 import {changeIconColor} from "/../scripts/social_icons.js";
 
@@ -8,8 +12,12 @@ window.addEventListener("DOMContentLoaded", init);
 function init() {
     console.log("init");
 
+    document.querySelector(".startTag").textContent = "";
+    document.querySelector(".endTag").textContent = "";
+
     gsap.registerPlugin(ScrollTrigger);
 
+    fadeInAnimation(startTag, endTag);
     burgerMenu();
     changeIconColor();
 }
