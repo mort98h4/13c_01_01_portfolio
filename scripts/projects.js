e6eaed17978beabb0198424ef059bdb7288ac7a8
Morten Gross <mg@vuffeli.dk>
2021-04-11 10:13:12 +0200

    added splash section to projects. changed names of sections.

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,14 +1,22 @@
 "use strict";
 
+const startTag = document.querySelector(".startTag").textContent;
+const endTag = document.querySelector(".endTag").textContent;
+
 import {burgerMenu} from "/../scripts/menu.js";
+import {fadeInAnimation} from "/scripts/header_animation.js";
 import {changeIconColor} from "/../scripts/social_icons.js";
 
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
     console.log("init");
+    document.querySelector(".startTag").textContent = "";
+    document.querySelector(".endTag").textContent = "";
 
     gsap.registerPlugin(ScrollTrigger);
+
+    fadeInAnimation(startTag, endTag);
 
     burgerMenu();
     changeIconColor();
